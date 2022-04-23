054676f82c7e99a259e52952a12cbf5f8eebb0f0
VeronikaMi <nika.mitichashili@gmail.com>
2022-04-23 16:00:30 +0400

    feat: table of merchants, dashboard redesign, line-chart left

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AGGREGATE, FACTS, FINDS } from 'src/app/interfaces';
 import { ApiService } from 'src/app/services/api.service';
-import { EChartsOption, time as eTime, number } from 'echarts';
+import { EChartsOption } from 'echarts';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -37,11 +38,21 @@ export class DashboardComponent implements OnInit {
     includes: ['dimension', 'date', 'volume'],
   };
 
+  public popularMerchants: AGGREGATE = {
+    dimension: 'merchant',
+    types: ['none'],
+    gteDate: '2018-01-01',
+    lteDate: '2018-01-31',
+    includeMetrics: ['volume'],
+  };
+
   public chartOptions: { [chart: string]: EChartsOption } = {
     donut: {},
     heatMap: {},
     line: {},
   };
+
+  public topMerchants!: FACTS[];
 
   constructor(private apiService: ApiService) {}
 
@@ -117,12 +128,13 @@ export class DashboardComponent implements OnInit {
             type: 'piecewise',
             orient: 'horizontal',
             left: '5',
-            bottom: 50,
+            top: 50,
           },
           calendar: {
-            top: 100,
+            bottom: 40,
             left: 30,
             right: 30,
+            orient: 'horizontal',
             cellSize: ['auto', 13],
             range: spending[0].dimension.substring(0, 4),
             itemStyle: {
@@ -143,24 +155,172 @@ export class DashboardComponent implements OnInit {
       });
 
     this.apiService
-      .findFactsBy(this.incomeByDate)
-      .subscribe((response: any) => {
-        console.log(response);
-        const income = response.entities;
-        const total = response.total;
-        console.log(Math.floor(total / 50));
-        for (let i = 0; i < Math.ceil(total / 50); ++i) {
-          this.apiService
-            .findFactsBy({
-              ...this.incomeByDate,
-              pageIndex: this.incomeByDate.pageIndex++,
-            })
-            .subscribe((response2: any) => {
-              income.push(...response2.entities);
-            });
-        }
-
-        console.log(income);
+      .aggregateTransactionFactsBy(this.popularMerchants)
+      .subscribe((response: FACTS[]) => {
+        console.log(Object.values(response));
+        const sortedMerchants = Object.values(response).sort(
+          (a, b) => b.volume - a.volume
+        );
+        this.topMerchants = sortedMerchants.slice(0, 20);
+        console.log(this.topMerchants);
       });
+
+    // do{}while(this.incomeByDate.pageIndex <= lastPage);?
+    // this.apiService
+    //   .findFactsBy(this.incomeByDate)
+    //   .subscribe((response: any) => {
+    //     console.log(response);
+    //     let income = response.entities;
+    //     const lastPage = Math.ceil((response.total - 50) / 50);
+    //     this.incomeByDate.pageIndex++;
+    //     const categories: string[] = [];
+    //     const days: string[] = [];
+
+    //     for (
+    //       ;
+    //       this.incomeByDate.pageIndex <= lastPage;
+    //       ++this.incomeByDate.pageIndex
+    //     ) {
+    //       this.apiService
+    //         .findFactsBy(this.incomeByDate)
+    //         .subscribe((response2: any) => {
+    //           income.push(...response2.entities);
+
+    //           // console.log(
+    //           income = income.map((fact: any) => ({
+    //             ...fact,
+    //             date: fact.date.substring(8, 10),
+    //           }));
+    //           // );
+    //           console.log(income);
+    //           let volumes = Array(31).fill(0);
+    //           const salary = income.filter(
+    //             (fact: any) => fact.dimension === 'Salary'
+    //           );
+    //           console.log(salary);
+
+    //           days.forEach((day: string) => {});
+
+    //           income.map(
+    //             (income: any) =>
+    //               !categories.includes(income.dimension) &&
+    //               categories.push(income.dimension)
+    //           );
+
+    //           income.map(
+    //             (income: any) =>
+    //               !days.includes(income.date.substring(0, 10)) &&
+    //               days.push(income.date.substring(0, 10))
+    //           );
+
+    //           this.chartOptions['line'] = {
+    //             title: {
+    //               text: 'Income',
+    //               textStyle: {
+    //                 color: '#2b2755',
+    //               },
+    //             },
+    //             tooltip: {
+    //               trigger: 'axis',
+    //               axisPointer: {
+    //                 type: 'cross',
+    //                 label: {
+    //                   backgroundColor: '#6a7985',
+    //                 },
+    //               },
+    //             },
+    //             legend: {
+    //               data: categories,
+    //             },
+    //             grid: {
+    //               left: '3%',
+    //               right: '4%',
+    //               bottom: '3%',
+    //               containLabel: true,
+    //             },
+    //             xAxis: [
+    //               {
+    //                 type: 'category',
+    //                 boundaryGap: false,
+    //                 data: days,
+    //               },
+    //             ],
+    //             yAxis: [
+    //               {
+    //                 type: 'value',
+    //               },
+    //             ],
+    //             series: [
+    //               // categories.forEach((category:string)=>(
+    //               //   {
+    //               //     name: category,
+    //               //     data: income.map(
+    //               //       (fact: any) =>
+    //               //         fact.dimension === category && fact.volume
+    //               //     ),
+    //               //   }
+    //               // ))
+    //               {
+    //                 name: categories[0],
+    //                 type: 'line',
+    //                 stack: 'Total',
+    //                 areaStyle: {},
+    //                 emphasis: {
+    //                   focus: 'series',
+    //                 },
+    //                 data: income.map(
+    //                   (fact: any) =>
+    //                     fact.dimension === categories[0] && fact.volume
+    //                 ),
+    //               },
+    //               {
+    //                 name: 'Union Ads',
+    //                 type: 'line',
+    //                 stack: 'Total',
+    //                 areaStyle: {},
+    //                 emphasis: {
+    //                   focus: 'series',
+    //                 },
+    //                 data: [220, 182, 191, 234, 290, 330, 310],
+    //               },
+    //               {
+    //                 name: 'Video Ads',
+    //                 type: 'line',
+    //                 stack: 'Total',
+    //                 areaStyle: {},
+    //                 emphasis: {
+    //                   focus: 'series',
+    //                 },
+    //                 data: [150, 232, 201, 154, 190, 330, 410],
+    //               },
+    //               {
+    //                 name: 'Direct',
+    //                 type: 'line',
+    //                 stack: 'Total',
+    //                 areaStyle: {},
+    //                 emphasis: {
+    //                   focus: 'series',
+    //                 },
+    //                 data: [320, 332, 301, 334, 390, 330, 320],
+    //               },
+    //               {
+    //                 name: 'Search Engine',
+    //                 type: 'line',
+    //                 stack: 'Total',
+    //                 label: {
+    //                   show: true,
+    //                   position: 'top',
+    //                 },
+    //                 areaStyle: {},
+    //                 emphasis: {
+    //                   focus: 'series',
+    //                 },
+    //                 data: [820, 932, 901, 934, 1290, 1330, 1320],
+    //               },
+    //             ],
+    //           };
+    //         });
+    //     }
+    //   });
   }
 }
