cf03aab9befce0356aa18f7d9fccc5e91e561467
VeronikaMi <nika.mitichashili@gmail.com>
2022-04-25 13:49:19 +0400

    refactor: unused import delete

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { AGGREGATE, FACTS, FactsResponse, FINDS } from '../interfaces';
 
 const API: string =
